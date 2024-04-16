bab866ad03f6b626655e4e57ba6a8ec64ebcc4f2
hyunmne <echo0913@naver.com>
2024-04-16 15:28:33 +0900

    spring-bankproj: Account 입출금, 계좌 생성, 계좌 조회, 전체계좌 조회 DAO

--- bankProj/src/main/java/com/kosta/bank/dao/AccountDao.java
+++ bankProj/src/main/java/com/kosta/bank/dao/AccountDao.java
@@ -1,7 +1,12 @@
 package com.kosta.bank.dao;
 
+import java.util.List;
+
 import com.kosta.bank.dto.Account;
 
 public interface AccountDao {
 	void insertAccount(Account acc) throws Exception;
+	Account selectAccount(String id) throws Exception;
+	void updateAccountBalance(String id, Integer balance) throws Exception;
+	List<Account> selectAccList() throws Exception;
 }
--- bankProj/src/main/java/com/kosta/bank/dao/AccountDaoImpl.java
+++ bankProj/src/main/java/com/kosta/bank/dao/AccountDaoImpl.java
@@ -1,5 +1,9 @@
 package com.kosta.bank.dao;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 
 import com.kosta.bank.dto.Account;
@@ -15,6 +19,24 @@ public class AccountDaoImpl implements AccountDao {
 	@Override
 	public void insertAccount(Account acc) throws Exception {
 		sqlSession.insert("mapper.account.insertAccount", acc);
+	}
+
+	@Override
+	public Account selectAccount(String id) throws Exception {
+	    return sqlSession.selectOne("mapper.account.selectAccount", id);
+	}
+
+	@Override
+	public void updateAccountBalance(String id, Integer balance) throws Exception {
+		Map<String, Object> param = new HashMap<>();
+		param.put("id", id);
+		param.put("balance", balance);
+		sqlSession.update("mapper.account.updateAccountBalance", param);
+	}
+
+	@Override
+	public List<Account> selectAccList() throws Exception {
+		return sqlSession.selectList("mapper.account.selectAccList");
 	}
 	
 }

b3b7402954860f921e0889e6d6d795cd05a7c88d
hyunmne <echo0913@naver.com>
2024-04-16 15:28:53 +0900

    spring-bankproj: Account 입출금, 계좌 생성, 계좌 조회, 전체계좌 조회 service

--- bankProj/src/main/java/com/kosta/bank/service/AccountServiceImpl.java
+++ bankProj/src/main/java/com/kosta/bank/service/AccountServiceImpl.java
@@ -1,5 +1,7 @@
 package com.kosta.bank.service;
 
+import java.util.List;
+
 import com.kosta.bank.dao.AccountDao;
 import com.kosta.bank.dto.Account;
 
@@ -13,7 +15,38 @@ public class AccountServiceImpl implements AccountService {
 
 	@Override
 	public void makeAccount(Account acc) throws Exception {
+		if (accDao.selectAccount(acc.getId())!=null) {
+			throw new Exception("계좌번호 중복 오류");
+		};
 		accDao.insertAccount(acc);
+	}
+
+	@Override
+	public Account accountInfo(String id) throws Exception {
+		Account acc = accDao.selectAccount(id);
+		if (acc == null) throw new Exception("계좌번호 오류");
+		return acc;
+	}
+
+	@Override
+	public void deposit(String id, Integer money) throws Exception {
+		Account acc = accDao.selectAccount(id);
+		if(acc==null) throw new Exception("계좌번호 오류");
+		acc.deposit(money);
+		accDao.updateAccountBalance(id, acc.getBalance());
+	}
+
+	@Override
+	public void withdraw(String id, Integer money) throws Exception {
+		Account acc = accDao.selectAccount(id);
+		if(acc==null) throw new Exception("계좌번호 오류");
+		acc.withdraw(money);
+		accDao.updateAccountBalance(id, acc.getBalance());
+	}
+
+	@Override
+	public List<Account> accList() throws Exception {
+		return accDao.selectAccList();
 	}
 
 }

f94f91678c37c523fb3ac2ecc24b206b222d0d23
hyunmne <echo0913@naver.com>
2024-04-16 15:28:43 +0900

    spring-bankproj: Account 입출금, 계좌 생성, 계좌 조회, 전체계좌 조회 controller

--- bankProj/src/main/java/com/kosta/bank/controller/AccountController.java
+++ bankProj/src/main/java/com/kosta/bank/controller/AccountController.java
@@ -1,9 +1,14 @@
 package com.kosta.bank.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kosta.bank.dto.Account;
 import com.kosta.bank.service.AccountService;
@@ -20,12 +25,95 @@ public class AccountController {
 	}
 	
 	@RequestMapping(value = "/makeAccount", method = RequestMethod.POST)
-	public String makeAccount(Account acc) {
+	public String makeAccount(Account acc, Model model) {
 		try {
 			accountService.makeAccount(acc);
+			model.addAttribute("acc", acc);
 			return "accountInfo";
 		} catch(Exception e) {
 			e.printStackTrace();
+			model.addAttribute("err", e.getMessage());
+			return "error";
+		}
+	}
+	
+	@RequestMapping(value="/accountInfo", method = RequestMethod.GET)
+	public String accountInfo() {
+		return "accountInfoForm";
+	}
+	
+	@RequestMapping(value="/accountInfo", method = RequestMethod.POST)
+	public ModelAndView accountInfo(@RequestParam("id") String id) {
+		// 모델과 뷰를 같이 줄 때
+		ModelAndView mav = new ModelAndView();
+		
+		try {
+			Account acc = accountService.accountInfo(id);
+			mav.addObject("acc", acc); // 데이터..
+			mav.setViewName("accountInfo"); // 이동페이지,.. ?
+		} catch(Exception e) {
+			e.printStackTrace();
+			mav.addObject("err", e.getMessage());
+			mav.setViewName("error");
+		}
+		return mav;
+	}
+	
+	@RequestMapping(value="/deposit", method = RequestMethod.GET)
+	public String Deposit() {
+		return "deposit";
+	}
+	
+	@RequestMapping(value="/deposit", method = RequestMethod.POST)
+	public ModelAndView Deposit(@RequestParam("id") String id, Integer money) {
+		// 모델과 뷰를 같이 줄 때
+		ModelAndView mav = new ModelAndView();
+		
+		try {
+			accountService.deposit(id, money);
+			Account acc = accountService.accountInfo(id);
+			mav.addObject("acc", acc); // 데이터..
+			mav.setViewName("accountInfo"); // 이동페이지,.. ?
+		} catch(Exception e) {
+			e.printStackTrace();
+			mav.addObject("err", e.getMessage());
+			mav.setViewName("error");
+		}
+		return mav;
+	}
+	
+	@RequestMapping(value="/withdraw", method = RequestMethod.GET)
+	public String Withdraw() {
+		return "withdraw";
+	}
+	
+	@RequestMapping(value="/withdraw", method = RequestMethod.POST)
+	public ModelAndView Withdraw(@RequestParam("id") String id, Integer money) {
+		// 모델과 뷰를 같이 줄 때
+		ModelAndView mav = new ModelAndView();
+		
+		try {
+			accountService.withdraw(id, money);
+			Account acc = accountService.accountInfo(id);
+			mav.addObject("acc", acc); // 데이터..
+			mav.setViewName("accountInfo"); // 이동페이지,.. ?
+		} catch(Exception e) {
+			e.printStackTrace();
+			mav.addObject("err", e.getMessage());
+			mav.setViewName("error");
+		}
+		return mav;
+	}
+	
+	@RequestMapping(value="/allAccountInfo", method = RequestMethod.GET)
+	public String allAccountInfo(Model model) {
+		try {
+			List<Account> accs = accountService.accList();
+			model.addAttribute("accs", accs);
+			return "allAccountInfo";
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("err", e.getMessage());
 			return "error";
 		}
 	}

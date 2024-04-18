2e1549aefdc0cd08311860b2a5601bb0f08b84de
hyunmne <echo0913@naver.com>
2024-04-18 10:24:46 +0900

    Spring-boardProj: boardlike service delete

--- boardProj/src/main/java/com/kosta/board/service/BoardLikeService.java
+++
@@ -1,6 +0,0 @@
-package com.kosta.board.service;
-
-public interface BoardLikeService {
-	boolean boardLike(String memberId, Integer boardNum) throws Exception;
-	boolean togleBrdLike(String memberId, Integer boardNum) throws Exception;
-}

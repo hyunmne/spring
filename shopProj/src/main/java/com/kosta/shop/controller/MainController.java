954fc632950a4f53aca7b9bebb54bbbccb53cfb7
hyunmne <echo0913@naver.com>
2024-04-18 17:25:22 +0900

    Spring-shopProj: 상품 조회

--- shopProj/src/main/java/com/kosta/shop/controller/MainController.java
+++ shopProj/src/main/java/com/kosta/shop/controller/MainController.java
@@ -1,13 +1,41 @@
 package com.kosta.shop.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kosta.shop.dto.Goods;
+import com.kosta.shop.service.GoodsService;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	private GoodsService gService;
 
 	@GetMapping("/")
-	public String main() {
+	public String index() {
 		return "index";
 	}
+	
+	@GetMapping("/main")
+	public ModelAndView main() {
+		ModelAndView mav = new ModelAndView();
+		
+		try {
+			List<Goods> goodsList = gService.goodsList();
+			mav.addObject("goods", goodsList);
+			mav.setViewName("shopMain");
+		} catch (Exception e) {
+			e.printStackTrace();
+			mav.addObject("action","상품목록 조회");
+			mav.addObject("message","상품 목록 조회 실패");
+			mav.setViewName("memberResult");
+		}
+		return mav;
+	}
+	
 }

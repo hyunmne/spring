7e444dcb2a5b9ab6d0e229fd62684aa1e9c94b1d
hyunmne <echo0913@naver.com>
2024-04-18 16:49:28 +0900

    Spring-shopProj: goods dto 생성

---
+++ shopProj/src/main/java/com/kosta/shop/dto/Goods.java
@@ -0,0 +1,53 @@
+package com.kosta.shop.dto;
+
+public class Goods {
+	private String gCode;
+	private String gCategory;
+	private String gName;
+	private String gContent;
+	private Integer gPrice;
+	private String gImage;
+
+	public String getgCode() {
+		return gCode;
+	}
+	public void setgCode(String gCode) {
+		this.gCode = gCode;
+	}
+	public String getgCategory() {
+		return gCategory;
+	}
+	public void setgCategory(String gCategory) {
+		this.gCategory = gCategory;
+	}
+	public String getgName() {
+		return gName;
+	}
+	public void setgName(String gName) {
+		this.gName = gName;
+	}
+	public String getgContent() {
+		return gContent;
+	}
+	public void setgContent(String gContent) {
+		this.gContent = gContent;
+	}
+	public Integer getgPrice() {
+		return gPrice;
+	}
+	public void setgPrice(Integer gPrice) {
+		this.gPrice = gPrice;
+	}
+	public String getgImage() {
+		return gImage;
+	}
+	public void setgImage(String gImage) {
+		this.gImage = gImage;
+	}
+	@Override
+	public String toString() {
+		return "Goods [gCode=" + gCode + ", gCategory=" + gCategory + ", gName=" + gName + ", gContent=" + gContent
+				+ ", gPrice=" + gPrice + ", gImage=" + gImage + "]";
+	}
+
+}
